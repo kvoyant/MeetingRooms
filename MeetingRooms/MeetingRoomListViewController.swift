9bb9baa8a9238c2d7c51961e57902fcf293f4d46
kvoyant <kvoyant@gmail.com>
2019-09-20 01:10:13 +0900

    Header & Footer 적용

--- MeetingRooms/MeetingRoomListViewController.swift
+++ MeetingRooms/MeetingRoomListViewController.swift
@@ -50,6 +50,14 @@ class MeetingRoomListViewController: UITableViewController {
         return cell
     }
     
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return Array(meetingRooms.keys)[section]
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        let rowCount = Array(meetingRooms.values)[section].count
+        return "\(rowCount) rooms"
+    }
 
     /*
     // Override to support conditional editing of the table view.
